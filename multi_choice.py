929c676901dc20212a0249eebfe210f0217f3655
Jujhar Singh <jujharsmc@gmail.com>
2022-04-06 13:22:32 +1200

    boilerplate

---
+++ multi_choice.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+
+class MultiChoiceGUI:
+    def __init__(self, parent):
+        root_frame = Frame(parent)
+        root_frame.grid()
+
+if __name__ == "__main__":
+    root = Tk()
+    MultiChoiceGUI(root)
+    root.mainloop()
