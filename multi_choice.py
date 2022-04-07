215485ec27731f02a874f11b17ccdbedc4461398
Jujhar Singh <jujharsmc@gmail.com>
2022-04-08 09:01:26 +1200

    add question class

--- multi_choice.py
+++ multi_choice.py
@@ -1,10 +1,31 @@
 from tkinter import *
 
+QUESTION = "Which of these is NOT a city?"
+
+ANSWERS = [
+    "England",
+    "New York",
+    "London",
+    "Barcelona"
+]
+
+CORRECT_ANSWER = ANSWERS.index("England")
 
 class MultiChoiceGUI:
     def __init__(self, parent):
         root_frame = Frame(parent)
         root_frame.grid()
+
+        self.question = MultiChoiceQuestion(QUESTION, ANSWERS, CORRECT_ANSWER)
+
+class MultiChoiceQuestion:
+    def __init__(self, question, answers, correct_answer):
+        self.question = question
+        self.answers = answers
+        self.correct_answer = correct_answer
+    
+    def is_correct_answer(self, selected):
+        return selected == self.correct_answer
 
 if __name__ == "__main__":
     root = Tk()
